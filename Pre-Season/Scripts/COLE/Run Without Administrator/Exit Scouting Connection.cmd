46f5eb04b5a29fca8bbaccd851e432556da2ec45
BuildTools <unconfigured@null.spigotmc.org>
2019-02-09 16:33:19 -0500

    Updated and Rearranged Files

--- Pre-Season/Scripts/COLE/Run Without Administrator/Exit Scouting Connection.cmd
+++
@@ -1,6 +0,0 @@
-REM NetSh Advfirewall set allprofiles state on
-REM Netsh Advfirewall show allprofiles
-REM netsh interface set interface "Wi-fi" enable
-netsh interface ip set address "Ethernet" dhcp
-netsh interface ip set address "Ethernet 1" dhcp
-pause
--- Pre-Season/Scripts/SCOUTING/Run With Admin/Start scouting connecton ADMIN.cmd
+++
@@ -1,5 +0,0 @@
-echo "Setting Ethernet adapters to static... Please wait.."
-netsh int ip set address "Ethernet" static 192.168.100.101 255.255.255.0 192.168.100.100
-netsh int ip set address "Ethernet 1" static 192.168.100.102 255.255.255.0 192.168.100.100
-runas netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
-pause
